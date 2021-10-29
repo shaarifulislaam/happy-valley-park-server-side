062bc0291d5ea07c83bf49d05475b7af61c94d86
Shariful Islam <sharifulmamun7@gmail.com>
2021-10-30 01:36:59 +0600

    all services get, single service get

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,11 +18,40 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-//run function
+//async function
 async function run() {
   try {
     await client.connect();
     console.log("connected with the server");
+    const ServicesCollection = client
+      .db("happyValleyPark")
+      .collection("services");
+    const UsersCollection = client.db("happyValleyPark").collection("users");
+
+    //*GET API or GET all services
+    app.get("/services", async (req, res) => {
+      const cursor = ServicesCollection.find({});
+      const services = await cursor.toArray();
+
+      res.send(services);
+    });
+
+    //*GET API for single services
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("getting specific service", id);
+      const query = { _id: ObjectId(id) };
+      const service = await ServicesCollection.findOne(query);
+
+      res.json(service);
+    });
+
+    //*ADD Services
+    app.post("/services", async (req, res) => {
+      const result = await ServicesCollection.insertOne(req.body);
+
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
