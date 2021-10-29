211f5e2393b9c0c8604fde8b16a7e5a869ecf179
Shariful Islam <sharifulmamun7@gmail.com>
2021-10-29 12:42:10 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -9,18 +9,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ip6hg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ip6hg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-// console.log(uri);
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}); */
+});
+
+//run function
+async function run() {
+  try {
+    await client.connect();
+    console.log("connected with the server");
+  } finally {
+    // await client.close();
+  }
+}
+//function call
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server running successfully!!");
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`Happy Valley Park server listening at http://localhost:${port}`);
 });
